d9214f0d5858d504144db935451cac89f53da75f
WiktorK02 <wiktor.kidon@hotmail.com>
2023-04-14 12:47:06 +0200

    Added preview image

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-from email.policy import default
 import tkinter as tk
 from tkinter import filedialog
 import cv2
 import numpy as np
 import customtkinter 
+from PIL import Image, ImageTk
 
 # To check extenxion
 import imghdr
 
 file_path = ''
+image_tk = ''
 
 def open_file():
     global file_path
     file_path = filedialog.askopenfilename()
     if file_path:
         file_label.configure(text="File: " + file_path)
-
     else:
         file_label.configure(text="No file selected")
 
 def generate_text():
-    global file_path
-
-    if not file_path:
-        output_text.delete(1.0, tk.END)
-        output_text.insert(tk.END, "No file selected")
-        return
+    global image_tk
 
     try:
         # To check extenxion
@@ -34,8 +29,7 @@ def generate_text():
         if extension == "png":
             # Load PNG image with alpha channel
             image = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
-
-            # Resize image to 128x64
+            # Resize the image to 128x64
             image = cv2.resize(image, (128, 64))
 
             # Get the alpha channel from the image
@@ -44,21 +38,43 @@ def generate_text():
             # Convert alpha values to 0 or 1 (0 if transparency == 0, 1 if not)
             binary_alpha = np.where(alpha_channel == 0, 0, 1)
 
+            # Convert the image to a PIL Image object and apply binary thresholding
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            image = Image.fromarray(image)
+
+            # Convert the PIL Image to a PhotoImage object
+            image_pre = ImageTk.PhotoImage(image)
+            
+            # Update the label's image
+            image_preview_label.config(image=image_pre)
+            image_preview_label.image = image_pre
+
         elif extension == "jpeg":
-            # Load JPEG image
+            # Load image
             image = cv2.imread(file_path, cv2.IMREAD_COLOR)
-
-            # Resize image to 128x64
+            
+            # Resize the image to 128x64
             image = cv2.resize(image, (128, 64))
-
+            
             # Convert RGB image to grayscale
             grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
             # Compute threshold value to convert grayscale to binary
             _, threshold_value = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
+            # Convert the image to a PIL Image object
+            image_pre = Image.fromarray(cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
+            
+            # Convert the PIL Image to a PhotoImage object
+            image_pre = ImageTk.PhotoImage(image_pre)
+            
+            # Update the label's image
+            image_preview_label.config(image=image_pre)
+            image_preview_label.image = image_pre
+                      
             # Convert grayscale image to binary alpha values
-            binary_alpha = np.where(grayscale_image <= threshold_value, 0, 1)
+            binary_alpha = np.where(grayscale_image <= threshold_value, 1, 0)
 
         # Reshape the binary alpha values into 1D array
         binary_alpha = binary_alpha.ravel()
@@ -85,6 +101,7 @@ def generate_text():
     except:
         output_text.delete(1.0, tk.END)
         output_text.insert(tk.END, "Error generating text")
+    
 
 def copy_all():
     global file_path
@@ -104,23 +121,23 @@ root = customtkinter.CTk()
 root.title("Image Converter v1.0")
 
 # Set window size
-root.geometry("320x450") 
+root.geometry("320x550") # Set width and height as desired
 
 # Label for "Open File" button
 open_file_label =  customtkinter.CTkLabel(root, text="Select a file:")
 open_file_label.grid(row=0, column=0, padx=10, pady=5, sticky="nsew")
 
 # "Open File" button
-open_file_button = customtkinter.CTkButton(root, text="Open File", command=open_file)
+open_file_button = customtkinter.CTkButton(root, text="Open Image File", command=open_file)
 open_file_button.grid(row=0, column=1, padx=10, pady=5, columnspan=1, sticky="nsew")
 
 # "Generate" button
-generate_button = customtkinter.CTkButton(root, text="Generate", command=generate_text)
+generate_button = customtkinter.CTkButton(root, text="Generate Code", command=generate_text)
 generate_button.grid(row=2, column=0, padx=10, pady=5, columnspan=1, sticky="nsew")
 
 # "Copy All" button
 copy_all_button = customtkinter.CTkButton(root, text="Copy All", command=copy_all)
-copy_all_button.grid(row=2, column=1, pady=5, padx=10, columnspan=1, sticky="nsew")  
+copy_all_button.grid(row=2, column=1, pady=5, padx=10, columnspan=1, sticky="nsew")  # Positioning the button on the right with fixed column value
 
 # Output text for
 output_text = customtkinter.CTkTextbox(root, height = 280, width = 300 )   # 25 50
@@ -134,5 +151,11 @@ copied_all_label.grid(row=4, column=0, padx=120, columnspan=3, sticky="nsew")
 file_label =  customtkinter.CTkLabel(root, text="No file selected")
 file_label.grid(row=5, column=0, padx=10, pady=5, columnspan=3, sticky="nsew")
 
-root.mainloop()
+# Label for "Open File" button
+preview_text_label =  customtkinter.CTkLabel(root, text="Preview:")
+preview_text_label.grid(row=6, column=0, padx=10, pady=5, sticky="nsew")
 
+# Create a label to display the image
+image_preview_label = tk.Label(root, bg='#242424')
+image_preview_label.grid(row=7, column=0, padx=10, pady=5, sticky="nsew")
+root.mainloop()
