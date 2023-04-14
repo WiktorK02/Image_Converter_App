dcefbf19ab9b92c8e3dd74dab78a8c5e62f9d5af
WiktorK02 <wiktor.kidon@hotmail.com>
2023-04-14 20:56:17 +0200

    Added reverse colors function and switch button

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ from tkinter import filedialog
 import cv2
 import numpy as np
 import customtkinter 
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
+import os
 
 # To check extenxion
 import imghdr
 
 file_path = ''
 image_tk = ''
+switch_state = False
 
 def open_file():
     global file_path
     file_path = filedialog.askopenfilename()
     if file_path:
-        file_label.configure(text="File: " + file_path)
+        file_label.configure(text="File: " + os.path.basename(file_path))
     else:
         file_label.configure(text="No file selected")
 
 def generate_text():
-    global image_tk
+    global image_tk, switch_var
 
     try:
         # To check extenxion
@@ -35,16 +37,22 @@ def generate_text():
             # Get the alpha channel from the image
             alpha_channel = image[:, :, 3]
 
-            # Convert alpha values to 0 or 1 (0 if transparency == 0, 1 if not)
-            binary_alpha = np.where(alpha_channel == 0, 0, 1)
-
+            if switch_state == True: 
+                # Convert alpha values to 0 or 1 (0 if transparency == 0, 1 if not)
+                binary_alpha = np.where(alpha_channel == 0, 1, 0)
+            else:
+                binary_alpha = np.where(alpha_channel == 0, 0, 1)
+            
             # Convert the image to a PIL Image object and apply binary thresholding
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            image = Image.fromarray(image)
-
+            grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            _, image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            image_pre = Image.fromarray(cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
+            
+            if switch_state == True:
+                    # Reverse the colors of the image
+                    image_pre = ImageOps.invert(image_pre)
             # Convert the PIL Image to a PhotoImage object
-            image_pre = ImageTk.PhotoImage(image)
+            image_pre = ImageTk.PhotoImage(image_pre)
             
             # Update the label's image
             image_preview_label.config(image=image_pre)
@@ -62,9 +70,13 @@ def generate_text():
 
             # Compute threshold value to convert grayscale to binary
             _, threshold_value = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
+            
             # Convert the image to a PIL Image object
             image_pre = Image.fromarray(cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
+            
+            if switch_state == True:
+                # Reverse the colors of the image
+                image_pre = ImageOps.invert(image_pre)
             
             # Convert the PIL Image to a PhotoImage object
             image_pre = ImageTk.PhotoImage(image_pre)
@@ -72,10 +84,12 @@ def generate_text():
             # Update the label's image
             image_preview_label.config(image=image_pre)
             image_preview_label.image = image_pre
-                      
-            # Convert grayscale image to binary alpha values
-            binary_alpha = np.where(grayscale_image <= threshold_value, 1, 0)
-
+        
+            if switch_state == True:   
+                # Convert grayscale image to binary alpha values
+                binary_alpha = np.where(grayscale_image <= threshold_value, 0, 1)
+            else:
+                binary_alpha = np.where(grayscale_image <= threshold_value, 1, 0)
         # Reshape the binary alpha values into 1D array
         binary_alpha = binary_alpha.ravel()
 
@@ -111,7 +125,11 @@ def copy_all():
 
     root.clipboard_clear()
     root.clipboard_append(output_text.get(1.0, tk.END))
-    copied_all_label.configure(text="✓ Copied All")
+    copied_all_label.configure(text="✓ Copied")
+
+def toggle_switch():
+    global switch_state
+    switch_state = not switch_state
 
 customtkinter.set_appearance_mode("System")  
 customtkinter.set_default_color_theme("blue")  
@@ -121,11 +139,11 @@ root = customtkinter.CTk()
 root.title("Image Converter v1.0")
 
 # Set window size
-root.geometry("320x550") # Set width and height as desired
+root.geometry("320x500") # Set width and height as desired
 
-# Label for "Open File" button
-open_file_label =  customtkinter.CTkLabel(root, text="Select a file:")
-open_file_label.grid(row=0, column=0, padx=10, pady=5, sticky="nsew")
+# Label for displaying selected file path
+file_label =  customtkinter.CTkLabel(root, text="No file selected")
+file_label.grid(row=0, column=0, padx=10, columnspan=1, sticky="nsew", pady=5)
 
 # "Open File" button
 open_file_button = customtkinter.CTkButton(root, text="Open Image File", command=open_file)
@@ -141,21 +159,25 @@ copy_all_button.grid(row=2, column=1, pady=5, padx=10, columnspan=1, sticky="nse
 
 # Output text for
 output_text = customtkinter.CTkTextbox(root, height = 280, width = 300 )   # 25 50
-output_text.grid(row=3, column=0, columnspan=3, padx=10, pady=5, sticky="nsew")
+output_text.grid(row=3, column=0, padx=10, pady=5, columnspan=3, sticky="nsew")
 
 # Label for displaying "Copied All" text
 copied_all_label =  customtkinter.CTkLabel(root, text="")
-copied_all_label.grid(row=4, column=0, padx=120, columnspan=3, sticky="nsew")
+copied_all_label.grid(row=4, column=1, pady=5,columnspan=3, sticky="nsew")
 
-# Label for displaying selected file path
-file_label =  customtkinter.CTkLabel(root, text="No file selected")
-file_label.grid(row=5, column=0, padx=10, pady=5, columnspan=3, sticky="nsew")
+# "Justify" text label center
+copied_all_label.grid_rowconfigure(0, weight=1)
+copied_all_label.grid_columnconfigure(0, weight=1)
 
 # Label for "Open File" button
-preview_text_label =  customtkinter.CTkLabel(root, text="Preview:")
-preview_text_label.grid(row=6, column=0, padx=10, pady=5, sticky="nsew")
+preview_text_label =  customtkinter.CTkLabel(root, text="Preview")
+preview_text_label.grid(row=4, column=0, padx=10, sticky="nsew", pady=5)
 
 # Create a label to display the image
 image_preview_label = tk.Label(root, bg='#242424')
-image_preview_label.grid(row=7, column=0, padx=10, pady=5, sticky="nsew")
+image_preview_label.grid(row=5, column=0, padx=10, sticky="nsew")
+
+# Switch label
+switch = customtkinter.CTkSwitch(root, text="Reverse colors", command=toggle_switch)
+switch.grid(row=1, column=0, padx=10, sticky="nsew")
 root.mainloop()
