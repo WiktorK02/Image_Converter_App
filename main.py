0add364dd17da216e31d9af1655709fe4501e4bf
WiktorK02 <wiktor.kidon@hotmail.com>
2023-04-15 16:06:11 +0200

    Updated: one function instead of two (extension)

--- main.py
+++ main.py
@@ -28,37 +28,7 @@ def generate_text():
         # To check extenxion
         extension = imghdr.what(file_path)
 
-        if extension == "png":
-            # Load PNG image with alpha channel
-            image = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
-            # Resize the image to 128x64
-            image = cv2.resize(image, (128, 64))
-
-            # Get the alpha channel from the image
-            alpha_channel = image[:, :, 3]
-
-            if switch_state == True: 
-                # Convert alpha values to 0 or 1 (0 if transparency == 0, 1 if not)
-                binary_alpha = np.where(alpha_channel == 0, 1, 0)
-            else:
-                binary_alpha = np.where(alpha_channel == 0, 0, 1)
-            
-            # Convert the image to a PIL Image object and apply binary thresholding
-            grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            _, image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            image_pre = Image.fromarray(cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
-            
-            if switch_state == True:
-                    # Reverse the colors of the image
-                    image_pre = ImageOps.invert(image_pre)
-            # Convert the PIL Image to a PhotoImage object
-            image_pre = ImageTk.PhotoImage(image_pre)
-            
-            # Update the label's image
-            image_preview_label.config(image=image_pre)
-            image_preview_label.image = image_pre
-
-        elif extension == "jpeg":
+        if  extension == "jpeg" or extension == "png":
             # Load image
             image = cv2.imread(file_path, cv2.IMREAD_COLOR)
             
@@ -67,13 +37,10 @@ def generate_text():
             
             # Convert RGB image to grayscale
             grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            # Compute threshold value to convert grayscale to binary
-            _, threshold_value = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             
             # Convert the image to a PIL Image object
             image_pre = Image.fromarray(cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
-            
+
             if switch_state == True:
                 # Reverse the colors of the image
                 image_pre = ImageOps.invert(image_pre)
@@ -85,16 +52,20 @@ def generate_text():
             image_preview_label.config(image=image_pre)
             image_preview_label.image = image_pre
         
-            if switch_state == True:   
-                # Convert grayscale image to binary alpha values
-                binary_alpha = np.where(grayscale_image <= threshold_value, 0, 1)
-            else:
-                binary_alpha = np.where(grayscale_image <= threshold_value, 1, 0)
-        # Reshape the binary alpha values into 1D array
-        binary_alpha = binary_alpha.ravel()
+        _, thresholded_image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        if switch_state == True:
+            # Convert the thresholded image to a binary array (0s and 1s)
+            binary_array = np.where(thresholded_image > 0, 0, 1)
+        else:
+            # Convert the thresholded image to a binary array (0s and 1s)
+            binary_array = np.where(thresholded_image > 0, 1, 0)
+
+        # Convert the binary array to a 1D array
+        one_d_array = binary_array.flatten()
 
         # Split the binary alpha values into 8-byte arrays
-        byte_arrays = np.packbits(binary_alpha)
+        byte_arrays = np.packbits(one_d_array)
 
         # Convert byte arrays to hexadecimal format
         hex_byte_arrays = ['0x' + format(byte, '02x') for byte in byte_arrays]
@@ -180,4 +151,5 @@ image_preview_label.grid(row=5, column=0, padx=10, sticky="nsew")
 # Switch label
 switch = customtkinter.CTkSwitch(root, text="Reverse colors", command=toggle_switch)
 switch.grid(row=1, column=0, padx=10, sticky="nsew")
+
 root.mainloop()
