d65c78c43cdd8bd6da0b1154e2f78cd6e196d307
wkidon <wiktorkidon@student.agh.edu.pl>
2023-04-21 20:31:04 +0200

    Ver update

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-DESCRIPTION = 'Image_Converter_App'
+REQUIREMENTS = [i.strip() for i in open("/home/wiktor/Image_Converter_App/requirements.txt").readlines()]
 LONG_DESCRIPTION = 'About Image converter GUI App to arduino oled display ssd1306 128x64'
 setup(
-   name='image_converter',
-   version='1.0',
-   description='Image converter GUI App to arduino oled display ssd1306 128x64',
+   name='image_to_arduino',
+   version='1.0.1',
+   description='Image converter to arduino',
    license="MIT",
    author='WiktorK02',
    author_email='wiktor.kidon@hotmail.com',
    url="https://github.com/WiktorK02/Image_Converter_App.git",
-   description=DESCRIPTION,
    long_description_content_type="text/markdown",
    long_description=LONG_DESCRIPTION,
-   packages=find_packages('src', exclude=['image_converter']),
+   packages=['src'],
+   package_data={'src': ['data/*.dat']},
    install_requires=REQUIREMENTS, 
 
 )
