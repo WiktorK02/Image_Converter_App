61d6c57e95736103c947cdc2d1937e6892337890
wkidon <wiktorkidon@student.agh.edu.pl>
2023-05-07 15:29:52 +0200

    Added entry points

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os
 
-REQUIREMENTS = [i.strip() for i in open("/home/wiktor/Image_Converter_App/requirements.txt").readlines()]
+
+# Get the absolute path to the directory containing this setup.py file
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Join the path to the requirements.txt file relative to the setup.py file
+req_file = os.path.join(here, 'requirements.txt')
+
+# Read the requirements.txt file and split into a list of lines
+with open(req_file, 'r') as f:
+    REQUIREMENTS = [line.strip() for line in f.readlines()]
 LONG_DESCRIPTION = 'About Image converter GUI App to arduino oled display ssd1306 128x64'
+
 setup(
    name='image-to-arduino',
-   version='1.0.3.2',
+   version='1.0.4.1',
    description='Image converter to arduino',
    license="MIT",
    author='WiktorK02',
@@ -15,5 +26,11 @@ setup(
    packages=['src'],
    package_data={'src': ['data/*.dat']},
    install_requires=REQUIREMENTS, 
+    entry_points={
+        'console_scripts': [
+            'image-to-arduino = src.imagetoarduino:main'
+        ]
+    },
 
 )
+
